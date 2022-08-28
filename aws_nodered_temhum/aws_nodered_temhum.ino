ab71d8fd4882f2cff3f3821d018c25a63ce4b76d
yejin0509 <yejin.moon59@gmail.com>
2022-08-28 15:11:33 +0900

    aws_nodered_연결

---
+++ aws_nodered_temhum/aws_nodered_temhum.ino
@@ -0,0 +1,43 @@
+//https://iter.kr/putty-install-on-windows/ putty설치
+//https://nodered.org/docs/getting-started/aws node-red putty 설치
+
+#include <EspMQTTClient.h> // PubSubclient 라이브러리를 포함한 ESP32 MQTT Client 라이브러리
+#include <DHT.h> //DHT11 라이브러리
+#define DHTTYPE DHT11 // DHT 센서 타입 정의
+DHT dht(13, DHTTYPE); //DHT 설정 dht( 핀, DHT 종류)
+float temp1,humid1;
+char temp[10]; // temp, humid 를 문자열로 합쳐서 전송하기 위한 변수 정의
+char humid[10];
+EspMQTTClient client(
+"U+Net9048", // SSID
+"1DECJE#2A4", // PW
+"broker.mqttdashboard.com", // MQTT Broker 서버 IP
+"TEST", // 클라이언트 이름
+1883 // MQTT 포트번호
+);
+
+
+void setup()
+{
+Serial.begin(115200); //Initialize serial
+dht.begin();
+client.enableDebuggingMessages();
+client.enableLastWillMessage("TestClient/lastwill", "I am going offline");
+}
+void onConnectionEstablished(){
+}
+void loop() {
+client.loop();
+temp1 = dht.readTemperature(); // 온도를 읽어서 tem에 저장
+humid1 = dht.readHumidity(); // 습도를 읽어서 hum에 저장
+sprintf(temp, "%.2f" ,dht.readTemperature());
+sprintf(humid, "%.2f" ,dht.readHumidity());
+Serial.print(F("Humidity : "));
+Serial.println(humid1);
+Serial.print(F("Temperature : "));
+Serial.println(temp1);
+client.publish("mytopic/temp", temp);
+client.publish("mytopic/humid", humid);
+delay(5000);
+
+}
